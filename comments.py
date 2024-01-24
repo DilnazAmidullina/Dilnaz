4fb8e59d93fe169e6c3980c64f4f82f29cdc6a65
Dilnaz <d_amidullina@kbtu.kz>
2024-01-25 02:34:59 +0600

    Dilnaz

--- comments.py
+++ comments.py
@@ -1,40 +1,40 @@
-#example
+# #example
 
-#This is comment
-print("Hello, World!")
+# #This is comment
+# print("Hello, World!")
 
-#example
-print("Hello, World!") #This is comment
+# #example
+# print("Hello, World!") #This is comment
 
-#example
+# #example
 
-#print("Hello, World!")
-print("Cheers, Mate!")
+# #print("Hello, World!")
+# print("Cheers, Mate!")
 
-#multiline comments
+# #multiline comments
 
-#This is a comment
-#written in
-#more than just one line
-print("Hello, World!") 
+# #This is a comment
+# #written in
+# #more than just one line
+# print("Hello, World!") 
 
-#example
-"""
+# #example
+# """
 
-This is comment
-written in 
-more than just one line
+# This is comment
+# written in 
+# more than just one line
 
-"""
+# """
 
-print("Hello, World!")
+# print("Hello, World!")
 
-#exercise 1
-#This is comment
-"""
+# #exercise 1
+# #This is comment
+# """
 
-This is comment
-written in 
-more than just one line
+# This is comment
+# written in 
+# more than just one line
 
-"""
+# """
--- syntax.py
+++ syntax.py
@@ -1,46 +1,46 @@
-#HOME
+# #HOME
 
-#example
-print("Hello, world!")
+# #example
+# print("Hello, world!")
 
-#exercise 1
-print("Hello world")
+# #exercise 1
+# print("Hello world")
 
-#PYTHON SYNTAX
-#example
-if 5 > 2:
-    print("Five is greater than two!")
+# #PYTHON SYNTAX
+# #example
+# if 5 > 2:
+#     print("Five is greater than two!")
 
-#syntax error
-if 5 > 2:
-print("Five is greater tha two!")
+# #syntax error
+# if 5 > 2:
+# print("Five is greater tha two!")
 
-#example
-if 5 > 2:
-    print("Five is greater than two!")
-if 5 > 2:
-        print("Five is greater than two!")
+# #example
+# if 5 > 2:
+#     print("Five is greater than two!")
+# if 5 > 2:
+#         print("Five is greater than two!")
 
-#syntax error
-if 5 > 2:
-     print("Five is greater than two!")
-        print("Five is greater than two!")
+# #syntax error
+# if 5 > 2:
+#      print("Five is greater than two!")
+#         print("Five is greater than two!")
      
-#python variables
-x = 5
-y = "Hello, world!"
+# #python variables
+# x = 5
+# y = "Hello, world!"
 
-print(x)
-print(y)
+# print(x)
+# print(y)
 
-#comments
+# #comments
 
-#This is a comment.
-print("Hello, World!")
+# #This is a comment.
+# print("Hello, World!")
 
-#exercise 2
-if 5 > 2:
-     print("YES")
+# #exercise 2
+# if 5 > 2:
+#      print("YES")
 
 
 
