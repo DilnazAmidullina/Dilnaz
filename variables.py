46a8f3e4fb38249a4410cddd7404520ac81047e5
Dilnaz <d_amidullina@kbtu.kz>
2024-01-25 02:34:22 +0600

    Dilnaz

--- variables.py
+++ variables.py
@@ -1,13 +1,85 @@
 #example
-x = 5
-y = "John"
+# x = 5
+# y = "John"
 
-print(x)
-print(y)
+# print(x)
+# print(y)
 
-#example
-x = 4
-x = "Sally"
-print(x)
+# #example
+# x = 4
+# x = "Sally"
+# print(x)
 
-#
+# #example
+# x = str(3)   #x will be 3
+# y = int(3)   #y will be 3
+# x = float(3)  #z will be 3
+
+# #example
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# #example
+# x = "John"
+# print(x)
+# #double quotes are the same as single quotes:
+# x = 'John'
+# print(x)
+
+# #example
+# a = 4
+# A = "Sally"
+# print(a)
+# print(A)
+# #A will not overwrite a
+
+
+# #variable names
+
+# #example
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+
+# print(myvar)
+# print(my_var)
+# print(_my_var)
+# print(myVar)
+# print(MYVAR)
+# print(myvar2)
+
+# #illegal variable names
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# #camel case
+# myVariableName = "John"
+
+# #pascal case
+# MyVariableName = "John"
+
+# #snake case
+# my_variable_name = "John"
+
+
+# #assign multiple values
+
+# #example
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# #one value to multiple variables
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
