ccd17ca9bbf339db56564dd4c856174affa859fd
Dilnaz <d_amidullina@kbtu.kz>
2024-01-25 04:09:02 +0600

    Dilnaz

---
+++ datatypes.py
@@ -0,0 +1,221 @@
+# #example
+# x = 5
+# print(type(x))
+
+# #example
+# x = "Hello World"
+# print(x)
+# print(type(x))
+
+# #example
+# x = 20
+# print(x)
+# print(type(x))
+
+# #example
+# x = 20.5
+# print(x)
+# print(type(x))
+
+# #example
+# x = 1j
+# print(x)
+# print(type(x))
+
+# #example
+# x = ["apple", "banana", "cherry"]
+# print(x)
+# print(type(x))
+
+# #example
+# x = ("apple", "banana", "cherry")
+# print(x)
+# print(type(x))
+
+# #example
+# x = {"name" : "John", "age" : 36}
+# print(x)
+# print(type(x))
+
+# #example
+# x = range(6)
+# print(x)
+# print(type(x))
+
+# #example
+# x = frozenset({"apple", "banana", "cherry"})
+# print(x)
+# print(type(x))
+
+# #example
+# x = True
+# print(x)
+# print(type(x))
+
+# #example
+# x = {"apple", "banana", "cherry"}
+# print(x)
+# print(type(x))
+
+# #example
+# x = b"Hello"
+# print(x)
+# print(type(x))
+
+# #example
+# x = bytearray(5)
+# print(x)
+# print(type(x))
+
+# #example
+# x = memoryview(bytes(5))
+# print(x)
+# print(type(x))
+
+# #example
+# x = None
+# print(x)
+# print(type(x))
+
+# #example
+# x = str("Hello World")
+# print(x)
+# print(type(x))
+
+# #example
+# x = int(20)
+# print(x)
+# print(type(x))
+
+# #example
+# x = float(20.5)
+# print(x)
+# print(type(x))
+
+# #example
+# x = complex(1j)
+# print(x)
+# print(type(x))
+
+# #example
+# x = list(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x))
+
+# #example
+# x = tuple(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x))
+
+# #example
+# x = range(6)
+# print(x)
+# print(type(x))
+
+# #example
+# x = dict(name="John", age=36)
+# print(x)
+# print(type(x))
+
+# #example
+# x = set(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x))
+
+# #example
+# x = frozenset(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x))
+
+# #example
+# x = bool(5)
+# print(x)
+# print(type(x))
+
+# #example
+# x = bytes(5)
+# print(x)
+# print(type(x))
+
+# #example
+# x = bytearray(5)
+# print(x)
+# print(type(x))
+
+# #example
+# x = memoryview(bytes(5))
+# print(x)
+# print(type(x))
+
+
+# #exercise1
+# x = 5
+# print(type(x))
+
+# int
+
+# #exercise2
+# x = "Hello World"
+# print(type(x))
+
+# str
+
+# #exercise3
+# x = 20.5
+# print(type(x))
+
+# float
+
+# #exercise4
+# x = ["apple", "banana", "cherry"]
+# print(type(x))
+
+# list
+
+# #exercise5
+# x = ("apple", "banana", "cherry")
+# print(type(x))
+
+# tuple
+
+# #exercise6
+# x = {"name" : "John", "age" : 36}
+# print(type(x))
+
+# dict
+
+# #exercise7
+# x = True
+# print(type(x))
+
+# bool
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
