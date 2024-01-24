ee4fa1d72a974ea8d7a1950585b6f71047f206f6
Dilnaz <d_amidullina@kbtu.kz>
2024-01-25 01:28:51 +0600

    Dilnaz

---
+++ lab1.py
@@ -0,0 +1,31 @@
+#HOME
+
+#example
+print("Hello, world!")
+
+#exercise
+print("Hello world")
+
+#PYTHON SYNTAX
+#example
+if 5 > 2:
+    print("Five is greater than two!")
+
+#syntax error
+if 5 > 2:
+print("Five is greater tha two!")
+
+#example
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+#syntax error
+if 5 > 2:
+     print("Five is greater than two!")
+        print("Five is greater than two!")
+     
+
+
+
