1fb129f442a7ad84eb7d0ee93f50bd57dcf4f759
Dilnaz <d_amidullina@kbtu.kz>
2024-01-31 22:31:34 +0600

    Dilnaz

--- lab2.py
+++ lab2.py
@@ -130,6 +130,7 @@
 #the floor division // rounds the result down to the nearest whole number
 
 
+
 #assingment operators
 
 #example
@@ -195,6 +196,7 @@
 # print(x)
 
 
+
 # comparison operators
 
 # equal
@@ -228,6 +230,7 @@
 # print(x <= y)
 
 
+
 # logical operators
 
 # and
@@ -244,6 +247,7 @@
 # x = 5
 # print(not(x > 3 and x < 10))
 # returns False because not is used to reverse the result
+
 
 
 # identity operators
@@ -269,12 +273,115 @@
 
 
 
+# membership operators
+
+# in
+# x = ["apple", "banana"]
+# print("banana" in x)
+
+# not in
+# x = ["apple", "banana"]
+# print("pineapple" not in x)
 
 
 
+# bitwise operations
+
+# &  AND
+# print(6 & 3)
+
+# outputs 2
+
+#|  OR
+# print(6 | 3)
+
+# outputs 7
+
+# ^ XOR
+# print(6 ^ 3)
+
+# outputs 5
+# 0 0 0
+# 0 1 1
+# 1 0 1
+# 1 1 0
+
+# ~  NOT
+# print(~4)
+
+# outputs -4
+# 0 1 
+# 1 0
+
+# <<  zero fill left shift
+# print(3 << 2)
+
+# outputs 12
+
+"""
+Shift left by pushing zeros in from the right and let the leftmost bits fall off
+The << operator inserts the specified number of 0's (in this case 2) from the right and let the same amount of leftmost bits fall off:
+
+If you push 00 in from the left:
+ 3 = 0000000000000011
+becomes
+12 = 0000000000001100
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
+
+# >> signed right shift
+# print(8 >> 2)
+
+# outputs 2
+
+"""
+Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+The >> operator moves each bit the specified number of times to the right. Empty holes at the left are filled with 0's.
+
+If you move each bit 2 times to the right, 8 becomes 2:
+ 8 = 0000000000001000
+becomes
+ 2 = 0000000000000010
+
+Decimal numbers and their binary values:
+ 0 = 0000000000000000
+ 1 = 0000000000000001
+ 2 = 0000000000000010
+ 3 = 0000000000000011
+ 4 = 0000000000000100
+ 5 = 0000000000000101
+ 6 = 0000000000000110
+ 7 = 0000000000000111
+ 8 = 0000000000001000
+ 9 = 0000000000001001
+10 = 0000000000001010
+11 = 0000000000001011
+12 = 0000000000001100
+"""
 
 
 
+# operator precedence (order of operations)
+
+#example
+# print((6 + 3) - (6 + 3))
+
+#example
+print(100 + 5 * 3)
 
 
 
