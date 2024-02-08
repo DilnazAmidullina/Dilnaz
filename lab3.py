a7a3167473cb2e77ee8759b0f42fd3631627284a
Dilnaz <d_amidullina@kbtu.kz>
2024-02-08 09:42:33 +0600

    Dilnaz

--- lab3.py
+++ lab3.py
@@ -327,7 +327,7 @@
 # Create a python file and import some of the functions from the above 13 tasks and try to use them.
 
 # from lab3function import isPalindrome
-# print(isPalindrome) 
+# isPalindrome
 
 
 
